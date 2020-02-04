02a40486d93106d9d8b1af795f5e28537dc39621
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-04 10:24:51 +0100

    wrapped components inside PageContent

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Form from './Form'
+import PageContent from './PageContent'
 import './App.css'
 
 function App() {
   return (
-    <>
+    <PageContent>
       <Navbar />
       <Form />
-    </>
+    </PageContent>
   )
 }
 
