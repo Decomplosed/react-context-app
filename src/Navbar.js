4b0d4f1b7d2ea1b9a30d4705e55a0c9c5fe3d0da
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-04 09:12:59 +0100

    added InputBase

--- src/Navbar.js
+++ src/Navbar.js
@@ -29,6 +29,13 @@ class Navbar extends Component {
               <div className={classes.searchIcon}>
                 <SearchIcon />
               </div>
+              <InputBase
+                placeholder='Search...'
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput
+                }}
+              />
             </div>
           </Toolbar>
         </AppBar>
