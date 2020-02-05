fc7fc16799bfd9f6f2a23b962d5b083d7ed5749b
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-05 23:13:24 +0100

    import LanguageCOntext

--- src/Form.js
+++ src/Form.js
@@ -14,6 +14,7 @@ import MenuItem from '@material-ui/core/MenuItem'
 import Select from '@material-ui/core/Select'
 import { withStyles } from '@material-ui/core/styles'
 import styles from './styles/FormStyles'
+import { LanguageContext } from './contexts/LanguageContext'
 
 class Form extends Component {
   render() {
