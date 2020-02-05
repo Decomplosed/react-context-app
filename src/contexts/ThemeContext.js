a84fccd5e556cbc4c11453f2d87a3c2823e73372
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-05 16:18:30 +0100

    created a class of ThemeProvider which render ThemeContext

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,3 +1,11 @@
 import React, { createContext } from 'react'
 
 const ThemeContext = createContext()
+
+class ThemeProvidec extends Comment {
+  render() {
+    <ThemeContext.Provider>
+      
+    </ThemeContext.Provider>
+  }
+}
