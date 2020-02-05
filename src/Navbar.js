2e3c6b99563ce6dfd6ee99a30102e06b753b5626
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-05 23:32:15 +0100

    wrapped entire layout inside LanguageContext.Consumer

--- src/Navbar.js
+++ src/Navbar.js
@@ -18,32 +18,43 @@ class Navbar extends Component {
     const { classes } = this.props
 
     return (
-      <div className={classes.root}>
-        <AppBar position='static' color={isDarkMode ? 'default' : 'primary'}>
-          <Toolbar>
-            <IconButton className={classes.menuButton} color='inherit'>
-              <span>😛</span>
-            </IconButton>
-            <Typography className={classes.title} variant='h6' color='inherit'>
-              App Title
-            </Typography>
-            <Switch onChange={toggleTheme} />
-            <div className={classes.grow} />
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-              <InputBase
-                placeholder='Search...'
-                classes={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput
-                }}
-              />
-            </div>
-          </Toolbar>
-        </AppBar>
-      </div>
+      <LanguageContext.Consumer>
+        {value => (
+          <div className={classes.root}>
+            <AppBar
+              position='static'
+              color={isDarkMode ? 'default' : 'primary'}
+            >
+              <Toolbar>
+                <IconButton className={classes.menuButton} color='inherit'>
+                  <span>😛</span>
+                </IconButton>
+                <Typography
+                  className={classes.title}
+                  variant='h6'
+                  color='inherit'
+                >
+                  App Title
+                </Typography>
+                <Switch onChange={toggleTheme} />
+                <div className={classes.grow} />
+                <div className={classes.search}>
+                  <div className={classes.searchIcon}>
+                    <SearchIcon />
+                  </div>
+                  <InputBase
+                    placeholder='Search...'
+                    classes={{
+                      root: classes.inputRoot,
+                      input: classes.inputInput
+                    }}
+                  />
+                </div>
+              </Toolbar>
+            </AppBar>
+          </div>
+        )}
+      </LanguageContext.Consumer>
     )
   }
 }
