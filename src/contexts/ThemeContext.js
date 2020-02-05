ce396339da6baad23f07e673f9f22c6b663f3f84
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-05 16:30:52 +0100

    export ThemeCOntext

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,15 +1,17 @@
 import React, { createContext } from 'react'
 
-const ThemeContext = createContext()
+export const ThemeContext = createContext()
 
 class ThemeProvider extends Comment {
   constructor(props) {
     super(props)
-    this.state = {isDarkMode: true}
+    this.state = { isDarkMode: true }
   }
   render() {
-    <ThemeContext.Provider value={{...this.state}}>{this.props.children}</ThemeContext.Provider>
+    return (
+      <ThemeContext.Provider value={{ ...this.state }}>
+        {this.props.children}
+      </ThemeContext.Provider>
+    )
   }
 }
-
-export default ThemeProvider
