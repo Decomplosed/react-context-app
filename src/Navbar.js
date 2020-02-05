bb94070dd0b9010c88710ac08330307ab1154056
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-05 23:42:38 +0100

    implemented content based on which we implement different words and emojis based on choosen language

--- src/Navbar.js
+++ src/Navbar.js
@@ -11,18 +11,35 @@ import styles from './styles/NavbarStyles'
 import { ThemeContext } from './contexts/ThemeContext'
 import { withLanguageContext } from './contexts/LanguageContext'
 
+const content = {
+  english: {
+    search: 'Search',
+    flag: '🇬🇧'
+  },
+  french: {
+    search: 'Chercher',
+    flag: '🇫🇷'
+  },
+  spanish: {
+    search: 'Buscar',
+    flag: '🇪🇸'
+  }
+}
+
 class Navbar extends Component {
   static contextType = ThemeContext
   render() {
     const { isDarkMode, toggleTheme } = this.context
     const { classes } = this.props
+    const { language } = this.props.languageContext
+    const { search, flag } = content[language]
 
     return (
       <div className={classes.root}>
         <AppBar position='static' color={isDarkMode ? 'default' : 'primary'}>
           <Toolbar>
             <IconButton className={classes.menuButton} color='inherit'>
-              <span>😛</span>
+              <span>{flag}</span>
             </IconButton>
             <Typography className={classes.title} variant='h6' color='inherit'>
               App Title
@@ -34,7 +51,7 @@ class Navbar extends Component {
                 <SearchIcon />
               </div>
               <InputBase
-                placeholder='Search...'
+                placeholder={`${search}...`}
                 classes={{
                   root: classes.inputRoot,
                   input: classes.inputInput
