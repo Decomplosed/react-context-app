e481e92ff52419a938e3eebe289de21eed9649ff
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-05 16:25:31 +0100

    created a constructor with state which include isDarkMode

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -3,6 +3,10 @@ import React, { createContext } from 'react'
 const ThemeContext = createContext()
 
 class ThemeProvider extends Comment {
+  constructor(props) {
+    super(props)
+    this.state = {isDarkMode: true}
+  }
   render() {
     <ThemeContext.Provider>{this.props.children}</ThemeContext.Provider>
   }
