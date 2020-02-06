4e762b8a28ae66d5fad792b41b7201cc94a53c6d
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-06 21:07:11 +0100

    removed toggleTheme method

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -6,10 +6,6 @@ export const ThemeContext = createContext()
 export function ThemeProvider(props) {
   const [isDarkMode, toggle] = useToggleState(false)
 
-  toggleTheme() {
-    this.setState({ isDarkMode: !this.state.isDarkMode })
-  }
-
     return (
       <ThemeContext.Provider
         value={{ ...this.state, toggleTheme: this.toggleTheme }}
