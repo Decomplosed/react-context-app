5f59796e7d6957fa6b25bc118570548e00b61fec
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-06 20:58:18 +0100

    pass props to the function and refer to props directly as this does not exist

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -2,13 +2,13 @@ import React, { createContext, useState } from 'react'
 
 export const LanguageContext = createContext()
 
-export function LanguageProvider() {
+export function LanguageProvider(props) {
   const [language, setLanguage] = useState('english')
   const changeLanguage = e => setLanguage(e.target.value)
 
   return (
     <LanguageContext.Provider value={{ language, changeLanguage }}>
-      {this.props.children}
+      {props.children}
     </LanguageContext.Provider>
   )
 }
